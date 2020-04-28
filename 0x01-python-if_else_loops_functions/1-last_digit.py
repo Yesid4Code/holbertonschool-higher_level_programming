40ce608b828e268212729faa35858fb4860c089b
Yesid A. López V <yesid4IT@gmail.com>
2020-04-28 18:15:07 +0000

    Changing Capital letters

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ else:
 if n > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, n))
 elif n == 0:
-    print("last digit of {} is {} and is 0".format(number, n))
+    print("Last digit of {} is {} and is 0".format(number, n))
 else:
-    print("last digit of {} is {} and is less than 6 and not 0"
+    print("Last digit of {} is {} and is less than 6 and not 0"
           .format(number, n))
