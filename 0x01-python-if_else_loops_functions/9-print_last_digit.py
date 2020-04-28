88b2d294415b2eace1821698fa5a9ec80ca7623f
Yesid A. López V <yesid4IT@gmail.com>
2020-04-28 21:49:14 +0000

    Adding a negative sign on the line 4

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        a = -number % 10
+        a = -(-number % 10)
     else:
         a = number % 10
         print(a, end="")
