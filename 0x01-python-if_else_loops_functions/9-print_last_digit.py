a4120e41355a419fabb52d03ce8628b238b76e7e
Yesid A. López V <yesid4IT@gmail.com>
2020-04-28 21:43:04 +0000

    adding conditions and printing the las digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        return ((-number) % 10)
-    return (number % 10)
+        a = -number % 10
+    else:
+        a = number % 10
+        print(a)
+    return (a)
