511928d80215f525dd4090fe1cbfba400ef8e8e3
Yesid A. López V <yesid4IT@gmail.com>
2020-04-28 20:06:48 +0000

    Print unique numbers

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for x in range(90):
+    if (x / 10) < (x % 10):
+        if x < 89:
+            print("{:02d}, ".format(x), end="")
+        else:
+            print("{:02d}".format(x))
