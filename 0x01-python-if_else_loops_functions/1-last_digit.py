9a24646be125e60d63bd593abaef132d8dbb25b8
Yesid A. López V <yesid4IT@gmail.com>
2020-04-28 17:15:29 +0000

    Script that print number

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+if number < 0:
+    n = -((-1 * number) % 10)
+else:
+    n = number % 10
+if n > 5:
+    print("Last digit of {:d} is {} and is greater than {}"\
+          .format(number, n, 5, 0))
+elif n == 0:
+    print("las digit of {} is {} and is {}".format(number, n, 0))
+else:
+    print("las digit of {} is {} and is less than {} and not {}"\
+          .format(number, n, 6, 0))
