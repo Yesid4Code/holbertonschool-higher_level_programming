652e7692548a82c95e86114f6dea1f5c783a7168
Yesid A. López V <yesid4IT@gmail.com>
2020-04-28 22:50:14 +0000

    Function that print the FizzBuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fuzzbuzz():
+    for x in range(1, 101):
+        out = ""
+        if (x % 3) == 0:
+            out += "Fizz"
+        if (x % 5) == 0:
+            out += "Buzz"
+
+        if out == "":
+            out = x
+        print(out, end=" ")
+    print("")
