052910145748e60438f5b9ef508da861377321df
Yesid A. López V <yesid4IT@gmail.com>
2020-04-28 22:00:47 +0000

    Get the print out of the condition

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        a = -(-number % 10)
+        a = (-1 * number) % 10
     else:
         a = number % 10
-        print(a, end="")
+    print(a, end="")
     return (a)
