3ea1da48a4f2da645d0b9e20d4f48b8a99446f8b
Yesid A. López V <yesid4IT@gmail.com>
2020-05-01 03:15:17 +0000

    add script that print the argv

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+
     import sys
-    if len(sys.argv) == 1:
-        print("{} arguments.".format(len(sys.argv) - 1))
-    elif len(sys.argv) == 2:
-        print("{} argument:".format(len(sys.argv) - 1))
+    len_argv = len(sys.argv)
+    if len_argv == 1:
+        print("0 arguments.")
+    elif len_argv == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
     else:
-        print("{} arguments:".format(len(sys.argv) - 1))
-    for i in range(len(sys.argv)):
-        if i == 0:
-            continue
-        print("{:d}: {:s}".format(i, sys.argv[i]))
+        print("{:d} arguments:".format(len_argv - 1))
+        for i in range(1, len_argv):
+            print("{:d}: {}".format(i, sys.argv[i]))
