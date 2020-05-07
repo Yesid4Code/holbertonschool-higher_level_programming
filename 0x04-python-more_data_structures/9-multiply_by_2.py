f2f6ee46852f87f1df75fd05dd45f9b205b303fd
Yesid A. López V <yesid4IT@gmail.com>
2020-05-07 21:35:41 +0000

    Change the value of the loop

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     my_dictionary = a_dictionary.copy()
-    for x, y in my_dictionary.items():
-        my_dictionary[x] = my_dictionary[y] * 2
+    for i in my_dictionary:
+        my_dictionary[i] = my_dictionary[i] * 2
     return my_dictionary
