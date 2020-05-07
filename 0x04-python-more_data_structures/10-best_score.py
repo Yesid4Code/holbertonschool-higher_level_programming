fec90a8bf4d3bd704df19e5d8bd9457a0c75b2b8
Yesid A. López V <yesid4IT@gmail.com>
2020-05-07 22:14:09 +0000

    Script that return the maximum value of a dictionary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    x = values(a_dictionary)
-    sorted(x)
-    return x[-1]
+    a = 0
+    for x in a_dictionary:
+        if a < a_dictionary[x]:
+            a = a_dictionary[x]
+    return a
