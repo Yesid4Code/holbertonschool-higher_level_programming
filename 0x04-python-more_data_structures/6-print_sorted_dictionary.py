44ff742cc25ccd680754e905f8f7860509297c1e
Yesid A. López V <yesid4IT@gmail.com>
2020-05-07 19:45:16 +0000

    Add format to print function

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for i in sorted(a_dictionary.keys):
-        print(i, end=" ")
-    print()
+        print("{}: {}".format(i, a_dictionary[i]))
