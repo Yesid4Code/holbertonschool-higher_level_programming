a3a134b2a8d4ea80b895b8eaed38d4f23213075c
Yesid A. López V <yesid4IT@gmail.com>
2020-05-07 23:31:56 +0000

    iterations variables changed

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,20 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    a = 0
-    for x in a_dictionary:
-        if a < a_dictionary[x]:
-            a = a_dictionary[x]
-    return a
+    if a_dictionary:
+        i = 0
+        for key, value in a_dictionary.items():
+            if i < value:
+                i = value
+                j = key
+        return j
+    return None
+
+# return (max(a_dictionary, key=a_dictionary.get) if a_dictionary else None)
+
+#  2 def best_score(a_dictionary):
+#  3     if a_dictionary:
+#  4         for key, value in a_dictionary.items():
+#  5             i = max(a_dictionary.values())
+#  6             if value == i:
+#  7                 return key
+#  8     return None
