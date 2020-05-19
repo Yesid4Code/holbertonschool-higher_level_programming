7ba4a1b073b0401f7a84bf6b0ec28c7236f57ffc
Yesid A. López V <yesid4IT@gmail.com>
2020-05-19 23:22:39 +0000

    Script that defines a Class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class Square:
+        """ A Square class definition """
+        def __init__(self, size):
+                """ Initialization of the class Square with size """
+                self.size = size
