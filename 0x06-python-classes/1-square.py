701f828739863789f1efedc11363c207ca097686
Yesid A. López V <yesid4IT@gmail.com>
2020-05-19 23:29:07 +0000

    change the scope of the instance size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -3,4 +3,4 @@ class Square:
         """ A Square class definition """
         def __init__(self, size):
                 """ Initialization of the class Square with size """
-                self.size = size
+                self.__size = size
