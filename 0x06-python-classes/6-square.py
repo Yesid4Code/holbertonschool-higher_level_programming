78fcef0e0f98e0669830f68878a4487072a304fe
Yesid A. López V <yesid4IT@gmail.com>
2020-05-20 04:37:47 +0000

    Condition on print square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -45,7 +45,7 @@ class Square:
 
         def my_print(self):
                 """ prints in stdout the square with the character # """
-                if self.__size > 0:
+                if self.__size > 0 and self.__position:
                         print("\n" * self.__position[1], end="")
                         for i in range(self.__size):
                                 print(" " * self.__position[0], end="")
