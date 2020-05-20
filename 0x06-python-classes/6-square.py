5fa690e6c5e961775bffa7f05350d12458e45cb1
Yesid A. López V <yesid4IT@gmail.com>
2020-05-20 04:09:17 +0000

    Add a condition of TypeError on init method

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -7,9 +7,9 @@ class Square:
         def __init__(self, size=0, position=(0, 0)):
                 """ Initialization of the class """
                 self.__size = size
-                if type(value) is not tuple or len(value) != 2 or \
-                   type(value[0]) is not int or value[0] < 0 or\
-                   type(value[1]) is not int or value[1] < 0:
+                if type(position) is not tuple or len(position) != 2 or \
+                   type(position[0]) is not int or position[0] < 0 or\
+                   type(position[1]) is not int or position[1] < 0:
                         raise TypeError("position must be\
                         a tuple of 2 positive integers")
                 self.__position = position
