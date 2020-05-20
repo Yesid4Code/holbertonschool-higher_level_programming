ad5acb4eb685996a45ba8ba2d3fd422fc1128586
Yesid A. López V <yesid4IT@gmail.com>
2020-05-20 01:08:08 +0000

    Change the name of a variable in the setter method

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -20,8 +20,8 @@ class Square:
         @size.setter
         def size(self, value):
                 """ Set the size of the square and check if it's >= 0  """
-                if type(size) is not int:
+                if type(value) is not int:
                         raise TypeError("size must be an integer")
-                if size < 0:
+                if value < 0:
                         raise ValueError("size must be >= 0")
                 self.__size = value
