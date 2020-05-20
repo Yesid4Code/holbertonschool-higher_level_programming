b48c1d0dd57c24891796595420218fbbfdfa777b
Yesid A. López V <yesid4IT@gmail.com>
2020-05-20 05:05:01 +0000

    change conditions on position setter method

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -35,8 +35,8 @@ class Square:
         @position.setter
         def position(self, value):
                 """ Set the position to print """
-                if type(value) is not tuple or len(value) != 2 or\
-type(value[0]) is not int or value[0] < 0 or\
+                if type(value) is not tuple or len(value) != 2 or
+type(value[0]) is not int or value[0] < 0 or
 type(value[1]) is not int or value[1] < 0:
                         raise TypeError("position must be a \
 tuple of 2 positive integers")
