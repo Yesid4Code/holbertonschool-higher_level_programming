dc38d33555f8a45ef120fbd2fbc9982d92f06ea4
Yesid A. López V <yesid4IT@gmail.com>
2020-05-20 04:42:45 +0000

    change

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -39,7 +39,7 @@ class Square:
                    type(value[0]) is not int or value[0] < 0 or\
                    type(value[1]) is not int or value[1] < 0:
                         raise TypeError("position must be a\
-                        tuple of 2 positive integers")
+tuple of 2 positive integers")
                 else:
                         self.__position = value
 
