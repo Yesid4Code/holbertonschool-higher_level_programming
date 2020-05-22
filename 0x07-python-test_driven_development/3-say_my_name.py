1835e6150f7fd5d37b5dbb60bee771b2d308bedc
Yesid A. López V <yesid4IT@gmail.com>
2020-05-22 22:48:08 +0000

    Print full name - Script

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''
+    xx
+'''
+def say_my_name(first_name, last_name=""):
+    if type(first_name) is str:
+        if type(last_name) is str:
+            print("My name is {:s} {:s}".format(first_name, last_name))
+        else:
+            raise TypeError("last_name must be a string")
+    else:
+        raise TypeError("first_name must be a string")
