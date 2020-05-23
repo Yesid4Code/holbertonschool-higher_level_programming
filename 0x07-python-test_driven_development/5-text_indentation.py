9cc7e173b6cdaa21565f332118867c3643527a69
Yesid A. López V <yesid4IT@gmail.com>
2020-05-23 02:51:31 +0000

    printing new line after special characters in a text

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''
+    d
+'''
+
+
+def text_indentation(text):
+    '''
+        This function automatically ident text
+        Args:
+            param1: text to ident.
+        Return:
+            Nothin.
+        Raises:
+            TypeError: if text is not a string.
+    '''
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    special_ch = [':', '.', '?']
+    flag = True
+
+
+    for c in text:
+        if c == " " and flag is True:
+            continue
+        if c in special_ch:
+            print(c, end="")
+            print("\n")
+            flag = True
+        else:
+            print(c, end="")
+            flag = False
