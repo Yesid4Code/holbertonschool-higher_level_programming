126e2e663617dca17fb2c6531c2f3ac8442bb876
Yesid A. López V <yesid4IT@gmail.com>
2020-05-23 05:14:38 +0000

    delete a line

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,19 +10,10 @@ class TestMaxInteger(unittest.TestCase):
     '''
     def test_max_integer(self):
         ''' cases '''
-        self.assertEqueal(max_integer([1, 2, 3]), 3)
-        self.assertEqueal(max_integer([-5, -30, -15, -23]), -5)
-        self.assertEqueal(max_integer([3.4, -2.1]), 3.4)
-        self.assertEqueal(max_integer([]), None)
-        self.assertEqueal(max_integer(5, 5, 5, 5), 5)
-        self.assertEqueal(max_integer([1.3, 23, 10, 90.1, 90.2]), 90.2)
-
-    def test_with_strints(self):
-        ''' cases with strings '''
-        self.assertEqueal(max_integer("holberton", 't'))
-        self.assertEqueal(max_integer("ABabcCZz234"), 'z')
-        self.assertEqueal(max_integer('a', 'b', 'c', 'd'), 'd')
-
-    def test_with_errors(self):
-        self.assertRaises(TypeError, max_integer, [5j, 3j])
-        self.assertRaises(TypeError, max_integer, 25)
+        self.assertEqual(max_integer([1, 2, 3]), 3)
+        self.assertEqual(max_integer([-5, -30, -15, -23]), -5)
+        self.assertEqual(max_integer([3.4, -2.1]), 3.4)
+        self.assertEqual(max_integer([5, 5, 5, 5]), 5)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([89]), 89)
+        self.assertEqual(max_integer([1.3, 23, 10, 90.1, 90.2]), 90.2)
