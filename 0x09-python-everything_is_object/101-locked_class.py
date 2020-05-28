787a20c7256e396feed4ed7769e04ba3abb43d6b
Yesid A. López V <Yesid4IT@gmail.com>
2020-05-28 06:37:21 +0000

    Adding docstring's documentation

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Create a locked class """
 
+
 class LockedClass:
     """ A locked class that only lets the user dynamically create the instance.
     attribute 'first_name'"""
