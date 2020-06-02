3a181243eb237bc9827238ec2cf13f4a8783403b
Yesid A. López V <Yesid4IT@gmail.com>
2020-06-02 22:40:01 +0000

    Exception added

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Improve Geometry """
+
+
+class BaseGeometry:
+    """ Class BaseGeometry. """
+    def area(self):
+        """ raise an Exception """
+        raise Exception("area() is not implemented")
