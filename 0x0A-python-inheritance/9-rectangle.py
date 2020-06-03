3ae0070ff9e745666f3df0b66ce8ecc8935e2ce3
Yesid A. López V <Yesid4IT@gmail.com>
2020-06-03 03:12:28 +0000

    area() and __str__() defined on class Rectangle

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Integer validator
+Rectangle
+"""
+
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Class Rectangle with BaseGeometry as baseclass """
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ Return string """
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+        # return "[Rectangle] {}/{}".format(self.__width, self.__height)
