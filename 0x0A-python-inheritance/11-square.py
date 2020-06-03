007e58eac7bd893221bcb86ad4f12d1cb84501ed
Yesid A. López V <Yesid4IT@gmail.com>
2020-06-03 03:43:32 +0000

    Method __str__ added

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Square """
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """ Class Square with inheritance from Rectangle. """
+    def __init__(self, size):
+        """ Initialize size"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+        self.area()
+
+    def __str__(self):
+        """ return str. """
+        return "[Square] {}/{}".format(self.__width, self.__height)
