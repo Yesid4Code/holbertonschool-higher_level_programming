eb319f4de1b5d9883a72c64720b16f78b1806b35
Yesid A. López V <Yesid4IT@gmail.com>
2020-06-03 18:45:45 +0000

    Print file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Read a file """
+
+
+def read_file(filename=""):
+    """
+    Function that reads a file and prints to stdout
+    Arg: filename
+    """
+    with open("my_file_0.txt", encoding="utf-8") as my_file:
+        print(my_file.read())
