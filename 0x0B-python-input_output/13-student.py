1a16fda27381b6136866a2e571c026ecb0a07e60
Yesid A. López V <Yesid4IT@gmail.com>
2020-06-04 03:09:37 +0000

    Replace attributes on an instance

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Student to JSON with filter. """
+
+
+class Student:
+    """ Define a student """
+    def __init__(self, first_name, last_name, age):
+        """ Instation with first_name, last_name, age. """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Dictionary representation of a Student.
+        Return:
+            a dictionary representation of a Student instance
+            with specified attributes
+        """
+        if attrs is None:
+            return self.__dict__
+
+        dictt = {}
+        for i in attrs:
+            if i in self.__dict__:
+                dictt[i] = self.__dict__[i]
+        return dictt
+
+    def reload_from_json(self, json):
+        """ Replaces all attributes of the Student instance. """
+        for items in json:
+            self.__dic__[item] = json[item]
