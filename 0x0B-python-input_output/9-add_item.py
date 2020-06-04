b743a094f3da279b0dde0e6609f55c91b739cd32
Yesid A. López V <Yesid4IT@gmail.com>
2020-06-04 01:26:49 +0000

    name file on import fixed

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -4,7 +4,7 @@
 
 from sys import argv
 load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
-save_to_json_file = __import__("7-save_json_file").save_to_json_file
+save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
 
 
 filename = "add_item.json"
