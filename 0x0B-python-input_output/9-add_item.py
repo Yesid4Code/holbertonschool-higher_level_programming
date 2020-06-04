9f2bdb99de48bde960939149e17fc2206102311a
Yesid A. López V <Yesid4IT@gmail.com>
2020-06-04 01:20:42 +0000

    change the way to import functions

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -3,8 +3,8 @@
 
 
 from sys import argv
-import save_to_json_file from 7-save_to_json_file.py
-import load_from_json_file from 8-load_from_json_file.py
+load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("7-save_json_file").save_to_json_file
 
 
 filename = "add_item.json"
