0d629c99b2309c71e3a149add7cc7fd266c36949
Yesid A. López V <Yesid4IT@gmail.com>
2020-06-05 19:47:09 +0000

    test_base.py

---
+++ 0x0C-python-almost_a_circlels/tests/test_models/test_base.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+    Conteins tests for Base class.
+"""
+
+
+import unittest
+from models.base import Base
+# here should be more imports like inspect, pep8, json, base,
+
+
+class TestBase(unittest.TestCase):
+    """
+    Check:
+      Test to check the documentation and style of Base class on TestBaseDocs.
+    Class contining functions to run
+    multiple tests
+    """
+    def SetUp(self):
+        """ Method to set the start point """
+        Base.Base__no_objects = 0
+
+    def test_0_id(self):
+        """
+        Test to chec for id method.
+        """
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        b4 = Base(12)
+        b5 = Base()
+        b6 = Base("text")
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
+        self.assertEqual(b4.id, 12)
+        self.assertEqual(b5.id, 4)
+        self.assertEqual(b6.id, "text")
+
+
+    if __name__ == '__main__':
+        unittest.main()
