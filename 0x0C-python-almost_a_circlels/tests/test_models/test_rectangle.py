3ddabd0cedb561a75ff43093572270c020e1b466
Yesid A. López V <Yesid4IT@gmail.com>
2020-06-05 21:28:59 +0000

    Test for rectangle class

---
+++ 0x0C-python-almost_a_circlels/tests/test_models/test_rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Conteins tests for Rectangle class.
+"""
+
+
+import unittest
+from models.rectangle import Rectangle
+# here should be more imports like inspect, pep8, json, base,
+
+
+class TestRectangle(unittest.TestCase):
+    """
+    Check:
+      Test to check the documentation and style of Base class on TestBaseDocs.
+    Class contining functions to run
+    multiple tests
+    """
+    def SetUp(self):
+        """ Method to set the start point """
