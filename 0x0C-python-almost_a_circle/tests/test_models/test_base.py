f9603fa678e0b82182ef7c16b207e1170e300a76
Yesid A. López V <Yesid4IT@gmail.com>
2020-06-09 17:37:27 +0000

    deleted unnecessary test cases

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -7,7 +7,6 @@
 import unittest
 import json
 from models.base import Base
-from models.rectangle import Rectangle
 # here should be more imports like inspect, pep8, json, base,
 
 
@@ -26,34 +25,12 @@ class TestBase(unittest.TestCase):
         """
         Test to chec for id method.
         """
-        b1 = Base()
-        b2 = Base()
-        b3 = Base()
-        b4 = Base(12)
-        b5 = Base()
-        b6 = Base("text")
-        self.assertEqual(b1.id, 1)
-        self.assertEqual(b2.id, 2)
-        self.assertEqual(b3.id, 3)
-        self.assertEqual(b4.id, 12)
-        self.assertEqual(b5.id, 4)
-        self.assertEqual(b6.id, "text")
-
-    def test_15_to_json_string(self):  # Task 15
-        """ Test method to_json_string. """
-        d0 = {"id": 9, "width": 5, "height": 6, "x": 7, "y": 8}
-        d1 = {"id": 2, "width": 2, "height": 3, "x": 4, "y": 0}
-        r8 = Rectangle(9, 8, 8, 4)
-        d2 = r8.to_dictionary()
-        j_dic = Base.to_json_string([d0, d1])
-        p_dic = json.loads(j_dic)
-        self.assertEqual(p_dic, [d0, d1])
-        j_dic = Base.to_json_string([d0, d2])
-        p_dic = json.loads(j_dic)
-        self.assertEqual(p_dic, [d0, d2])
-        j_dic = Base.to_json_string(None)
-        self.assertEqual(j_dic, "[]")
-
+        b0 = Base()
+        b1 = Base(23)
+        b2 = Base("text")
+        self.assertEqual(b0.id, 1)
+        self.assertEqual(b1.id, 23)
+        self.assertEqual(b2.id, "text")
 
 if __name__ == '__main__':
         unittest.main()
