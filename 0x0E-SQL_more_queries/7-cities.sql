7a805e615375750eb1a606df86d9ecd591a868bd
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-07-14 22:32:35 +0000

    Auto_increment added

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,6 @@
 -- Create database with a table using key and foreign key
 CREATE DATABASE IF NOT exists hbtn_0d_usa;
 CREATE TABLE IF NOT exists hbtn_0d_usa.cities (
-id INT PRIMARY KEY NOT NULL,
+id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
 state_id INT FOREIGN KEY REFERENCES hbtn_0d_usa.states(id) NOT NULL,
 name VARCHAR(256) NOT NULL);
