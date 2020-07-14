c5ae9b5dce29fc4183ffb8b0555ccb054d81618a
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-07-14 23:05:34 +0000

    change how delcarate the foreign key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,5 +3,6 @@
 CREATE DATABASE IF NOT exists hbtn_0d_usa;
 CREATE TABLE IF NOT exists hbtn_0d_usa.cities (
 id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-state_id INT FOREIGN KEY REFERENCES hbtn_0d_usa.states(id) NOT NULL,
-name VARCHAR(256) NOT NULL);
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));
