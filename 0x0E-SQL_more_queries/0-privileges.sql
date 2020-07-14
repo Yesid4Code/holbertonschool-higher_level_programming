11658b8c8b2b3251621638bccde0b721dcc1db56
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-07-14 18:58:30 +0000

    add required second comment

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- script that list of user's provoleges
+-- script that list user's provoleges
+-- usefull for many users.
 SHOW GRANTS FOR user_0d_1@localhost;
 SHOW GRANTS FOR user_0d_2@localhost;
