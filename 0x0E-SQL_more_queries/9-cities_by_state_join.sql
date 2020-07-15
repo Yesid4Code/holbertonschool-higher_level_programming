2f324530a94407efd95f54a54976075f1beebe95
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-07-15 01:39:40 +0000

    name fixed

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
--- 
+-- Select from two tables using selec and join.
 SELECT cities.id, cities.name, states.name
 FROM cities INNER JOIN states
-ON cities.states_id = states.id
+ON cities.state_id = states.id
 ORDER BY cities.id ASC;
