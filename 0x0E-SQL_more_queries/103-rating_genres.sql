fb3428336be065869c0c94f96ba6178951e207e3
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-07-15 14:44:35 +0000

    change the code

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,6 +1,7 @@
 -- Script that lists all genres by thir rating.
-SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
-FROM tv_shows
-INNER JOIN tv_show_ratings ON tv_show_ratings.show_id = tv_shows.id
-GROUP BY tv_shows.title
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.name
 ORDER BY rating DESC;
