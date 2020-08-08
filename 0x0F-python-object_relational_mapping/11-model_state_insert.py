b4046d053fe2bebd48a213e43f365ba79a4c938a
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-08 23:18:56 +0000

    Print the id of new_state

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -26,4 +26,5 @@ if __name__ == "__main__":
     new_state = State(name='Louisiana')
     session.add(new_state)
     session.commit()  # emits the UPDATE statement
+    print(new_state.id)
     session.close()
