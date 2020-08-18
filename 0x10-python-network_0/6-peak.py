4f8f93076bcbf19de7b3fd8d59afe572c2d1a748
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-18 22:07:02 +0000

    other form

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,8 +5,15 @@
 def find_peak(list_of_integers):
     """ . """
     listt = list_of_integers.copy()
-    for idx in range(1, len(listt)):
-        if listt[idx] >= listt[idx - 1] and listt[idx] >= listt[idx + 1]:
-            return listt[idx]
+    for idx in range(len(listt)):
+        if (idx - 1) >= 0 and (idx + 1) <= (len(listt) - 1):
+            if listt[idx] >= listt[idx - 1] and listt[idx] >= listt[idx + 1]:
+                return listt[idx]
+        if (idx - 1) < 0:
+            if listt[idx] >= listt[idx + 1]:
+                return listt[idx]
+        if (idx + 1) > (len(listt) - 1):
+            if listt[idx] >= listt[idx - 1]:
+                return listt[idx]
 
     return None
