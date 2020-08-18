fa8fe11096e54be998d293fc819005eb8ded8f32
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-18 20:26:40 +0000

    fixed

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Script that displays the body of the response of a request.
-curl -sX POST -d "email=hr@holbertonschool.com&subje
-ct=I will always be here for PLD" "$1"
+curl -sX POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
