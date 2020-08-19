1a66247926e68007c456c95e9df0eacdfb39c0f7
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-19 03:56:20 +0000

    condition added

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,6 +8,8 @@ def find_peak(list_of_integers):
     length = len(list_of_integers)
     if length == 0:
         return
+    if length == 1:
+        return listt[0]
     pivot = length // 2
     if (pivot == 0 or listt[pivot] > listt[pivot - 1]) and\
        (pivot == length - 1 or listt[pivot] >= listt[pivot + 1]):
