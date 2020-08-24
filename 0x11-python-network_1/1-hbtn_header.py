62e152d3ab4eaef16087809d447cd0b043d35559
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-24 18:06:45 +0000

    commend fixed

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,5 +5,5 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    with urlopen(argv[1]) as rid:  #response ID
+    with urlopen(argv[1]) as rid:  # response ID
         print(rid.headers.get("X-Request-Id"))
