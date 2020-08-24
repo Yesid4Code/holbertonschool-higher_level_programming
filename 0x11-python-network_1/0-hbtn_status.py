753c085e0940ab9caa6c964815da0f88749b54dc
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-24 16:09:45 +0000

    idented

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" . """
+""" Status of a request. """
 from urllib.request import urlopen
 
 
 if __name__ == "__main__":
     with urlopen("https://intranet.hbtn.io/status") as response:
         html = response.read()
-    print("Body response")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode("utf-8")))
+        print("Body response")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
