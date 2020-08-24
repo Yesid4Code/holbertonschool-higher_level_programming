3803dc17707ab9597ef6cfbecfe9e61929b48fe5
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-24 23:10:38 +0000

    send a POST and display the response

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Script that send a POST request to the passed URL
+    Return the body of the response.
+"""
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from sys import argv
+
+
+if __name__ == "__main__":
+    try:
+        with urlopen(argv[1]) as body:
+            print(body.read().decode("utf-8"))
+    except HTTPError:
+        print("Error code: {}".format(HTTPError.code))
+
+
+# encode: when the result contains "str" data ==> "bytes" data
+# decode: when the result contains "bytes" data ==> "str" data
