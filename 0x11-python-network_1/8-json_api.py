a5c7fde478243c20df16f05135d56757f75c5af8
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-25 01:02:58 +0000

    POST request with parameters

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    Script that send a POST request to the passed URL
+    Return the id and the name from a JSON format.
+"""
+from requests import post
+from sys import argv
+import json
+
+
+if __name__ == "__main__":
+    try:
+        letter = argv[1]
+    except Exception:
+        letter = ""
+
+    try:
+        url = "http://0.0.0.0:5000/search_user"
+        req = post(url, data={"q":letter})
+        req_dic = req.json()
+        req_id = req_dic.get("id")
+        req_name = req_dic.get("name")
+        try:
+            print("[{}] {}".format(req_id, req_name))
+        except Exception:
+            print("No a result")
+    except Exception:
+        print("Not a valid JSON")
+
+
+# json() = decode
