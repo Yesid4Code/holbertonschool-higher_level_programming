eb8b30f7e413fcbbe44348faf4a3ac210a20dc0d
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-25 02:30:27 +0000

    's' added to requests

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,11 +3,11 @@
     Script that takes the Github credentials (username and password)
     Return the id using the Github API
 """
-import request
+import requests
 from sys import argv
 
 
 if __name__ == "__main__":
-    req = request.get("https://api.github.com/user", auth=(argv[1], argv[2])
+    req = requests.get("https://api.github.com/user", auth=(argv[1], argv[2])
     req_dic = req.json()  # decode
     print(name.get("id"))
