5d50cdc842c9a39367f5f0f661ff279e89f26332
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-08-25 01:30:06 +0000

    updated unnecesary import deleted: json swap the message of the if condition and the else

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -3,9 +3,8 @@
     Script that send a POST request to the passed URL
     Return the id and the name from a JSON format.
 """
-from requests import post, get
+from requests import post
 from sys import argv
-import json
 
 
 if __name__ == "__main__":
@@ -21,9 +20,9 @@ if __name__ == "__main__":
         req_id = req_dic.get("id")
         req_name = req_dic.get("name")
         if len(req_dic) == 0 or not req_id or not name:
-            print("[{}] {}".format(req_id, req_name))
+            print("No result")
         else:
-            print("No a result")
+            print("[{}] {}".format(req_id, req_name))
     except Exception:
         print("Not a valid JSON")
 
