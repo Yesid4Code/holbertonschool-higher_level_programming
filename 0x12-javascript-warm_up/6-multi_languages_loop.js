6113ec2b8d637e9f5feb12a465ea2c3abb1a9ba0
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-15 21:54:52 +0000

    undeclare idx

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/nodejs
 /* Script that prints 3 lines using an array of strings */
 const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let idx in array) {
-  console.log(array[idx]);
+for (const idx of array) {
+  console.log(idx);
 }
