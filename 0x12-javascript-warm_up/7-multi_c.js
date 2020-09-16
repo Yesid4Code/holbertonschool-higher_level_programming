2b368c04b54361fbf9801a25402f022a1097dd9f
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-16 00:23:35 +0000

    change the type of variable i, on line 4

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,7 +1,7 @@
 #!/usr/bin/nodejs
 /* Script that prints 3 lines using an array of strings */
 const x = parseInt(process.argv[2]);
-const i = 0;
+let i = 0;
 if (x) {
   while (i < x) {
     console.log('C is fun');
