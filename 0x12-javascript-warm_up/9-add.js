5e4659338228d049eff19f95f99627393564b09c
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-16 02:39:11 +0000

    add between two numbers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,10 @@
+#!/usr/bin/nodejs
+/**
+ * Scrip that prints a square
+ */
+const n1 = parseInt(process.argv[2]);
+const n2 = parseInt(process.argv[3]);
+
+if ((n1 && n2) === true) {
+    console.log(n1 + n2);
+}
