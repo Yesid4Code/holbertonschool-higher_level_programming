b3f7e7c121964861bb48c2b49dba945cdfe833b3
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-16 00:14:24 +0000

    declarete variable 'i'

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
-#!/usr/bin/node
+#!/usr/bin/nodejs
 /* Script that prints 3 lines using an array of strings */
 const x = parseInt(process.argv[2]);
-let i = 0;
+const i = 0;
 if (x) {
-  while (i < x) {
+  while (i > x) {
     console.log('C is fun');
     i++;
   }
