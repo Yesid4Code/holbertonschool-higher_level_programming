47fb170dfc018562f9f35393bf278ff19712da54
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-16 02:19:01 +0000

    print a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,10 +3,10 @@
  * Scrip that prints a square
  */
 const arg = parseInt(process.argv[2]);
-const m = 'x';
-let i = 0;
 if (arg) {
-  while (i < arg) {
-    console.log(x * arg);
+  for (const i = 0; i < arg; i++) {
+    console.log('X'.repeat(arg));
   }
+} else {
+  console.log('Missing size');
 }
