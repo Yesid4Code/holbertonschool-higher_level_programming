12cd1aaa613273d68b054a632db552c05bfefab3
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-16 03:58:16 +0000

    print the second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/nodejs
+/**
+ * Scrip that search the second biggest integer in a list of arguments
+ */
+const args = process.argv
+const length = args.length
+if (length > 2) {
+  args.sort()
+  console.log(args[length - 2]);
+} else {
+  console.log('0');
+}
