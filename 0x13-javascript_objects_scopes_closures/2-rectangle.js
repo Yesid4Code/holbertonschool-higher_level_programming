a05ca984e2dcd8eae520cf04cbdffbc541558d87
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-21 20:35:36 +0000

    adding a missing semicolon

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -5,7 +5,7 @@
 class Rectangle {
   constructor (w, h) {
     if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
-      return this
+      return this;
     }
     this.width = w;
     this.height = h;
