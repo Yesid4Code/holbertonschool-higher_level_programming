53c249b3b5b2a94ea1a29f72067bd5d1cdc175d1
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-21 20:33:47 +0000

    change the condition

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -4,12 +4,11 @@
  */
 class Rectangle {
   constructor (w, h) {
-    if (w > 0 || h > 0) {
-      this.width = w;
-      this.height = h;
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
       return this
     }
-    return this
+    this.width = w;
+    this.height = h;
   }
 }
 
