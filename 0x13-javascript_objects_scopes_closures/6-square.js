f3a102eebcec68e1a69c7b7cf72228d349e2e8d6
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-21 23:28:03 +0000

    scoop of the loop changed

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,12 +3,12 @@
  * Class that define a square and inherits from Rectangle of 4-rectangle.js
  */
 class Square extends require('./5-square') {
-  charPrint(c) {
+  charPrint (c) {
     if (c === undefined) {
       c = 'X';
     }
     let i;
-    for (i = 0; i < this.with; i++) {
+    for (i = 0; i < this.height; i++) {
       console.log(c.repeat(this.height));
     }
   }
