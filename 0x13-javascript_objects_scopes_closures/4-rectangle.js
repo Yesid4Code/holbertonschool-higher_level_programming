cdb8d7a018ca344eedfda8fa40f2b8f5282aa2b8
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-21 22:43:49 +0000

    class members added Rotate and double

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,33 @@
+#!/usr/bin/nodejs
+/**
+ * Class that define a rectangle and print a rectangle.
+ */
+class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return this;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    let i;
+    for (i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    let tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.higth *= 2;
+  }
+}
+
+module.exports = Rectangle;
