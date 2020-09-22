e01683f3552fd27621a60e89eef7bdbbc70412dc
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-22 00:03:53 +0000

    function that reverse a list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,14 @@
+#!/usr/bin/nodejs
+/**
+ * Function that returns the number of occurrences in a list.
+ */
+exports.esrever = function (list) {
+  let l = length;
+  let tmp = '';
+  for (let i = 0; i < l; i++, l--) {
+    tmp = list[i];
+    list[i] = list[l - 1];
+    list[l - 1] = tmp;
+  }
+  return list;
+};
