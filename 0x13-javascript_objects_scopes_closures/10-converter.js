faed58321f55d446c8f8bce4d548d0c5172ee45b
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-22 00:44:48 +0000

    function name exported fixed

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -3,8 +3,9 @@
  * Function that converts a number from base 1o to another.
  * The base is passed as argument:
  */
-exports.convert = function (base) {
+exports.converter = function (base) {
   function convert (number) {
     return (number.toString(base));
   }
+  return convert;
 };
