3f41e48dfccb654bbce4d436114d367510b110cf
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-23 03:37:46 +0000

    Change the structure. Use another way to write on the document.

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#!/usr/bin/nodejs
 /**
  * Script that gets the contents of a webpage
  * and stores it in a file.
@@ -6,4 +6,11 @@
 const fs = require('fs');
 const request = require('request');
 const url = process.argv[2];
-request(url, 'utf-8').pipe(fs.createWriteStream(process.argv[3]));
+const file = process.argv[3];
+request.get(url, function (err, response, body) {
+  if (err) {
+    console.log(err)
+  } else {
+    fs.writeFile(file, body, 'utf-8');
+  }
+});
