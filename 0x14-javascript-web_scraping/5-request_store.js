a71ae9a24a099b7765208e9b87e79dd5308f8f55
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-23 04:01:12 +0000

    ';' added

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -9,7 +9,7 @@ const url = process.argv[2];
 const file = process.argv[3];
 request.get(url, function (err, response, body) {
   if (err) {
-    console.log(err)
+    console.log(err);
   } else {
     fs.writeFile(file, body, 'utf-8');
   }
