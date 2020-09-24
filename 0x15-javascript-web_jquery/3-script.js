f248828d58efbba1e5e39dc9fe4fb52751739467
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-24 21:41:06 +0000

    id of the element changed

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,6 +1,6 @@
 /**
  * JS to add a class to HTML tag when user clicks on the tag DIV#red_header
  */
-$('#red_header').click(function () {
+$('DIV#red_header').click(function () {
   $('header').addClass('red');
 });
