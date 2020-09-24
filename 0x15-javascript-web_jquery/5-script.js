a35996330c8dacf4d924523f8ba3e9aa070daf00
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-24 21:45:46 +0000

    LI element added to a list

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,7 @@
+/**
+ * JS that adds a LI element to a list.
+ * The element is added when the user clicks on the tag DIV#add_item.
+ */
+$('DIV#add_item').click(function () {
+  $('UL.my_list').append('<li>Item</li>');
+}
