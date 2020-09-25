561b6f5190fde59f0ecffdce211da57991d02ccd
Yesid A. López V <54006744+Yesid4Code@users.noreply.github.com>
2020-09-25 00:50:33 +0000

    change de header text color

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,7 @@
+/**
+ * JS that updates the text color of the HTML tag HEADER to red
+ */
+document.addEventListener('DOMContentLoaded', function () {
+  // To runs when the DOM is ready, i.e. when the document has been parsed
+  document.querySelector('HEADER').style.color = '#FF0000';
+});
